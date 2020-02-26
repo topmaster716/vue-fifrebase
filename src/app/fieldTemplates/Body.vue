96160fbe7e60517510a2dc11a3a8c2fad51d5e51
Andrei Averyanov <top.master716@gmail.com>
2020-02-26 02:50:53 -0800

    Project: review

---
+++ src/app/fieldTemplates/Body.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-html="contentBody" :class="fieldClass"></div>
+</template>
+<script>
+export default {
+  props: {
+    body: '',
+    fieldClass:''
+  },
+  computed: {
+    contentBody: function() {
+      return this.body
+    }
+  }
+}
+</script>
+<style lang="scss">
+// here you can add custom styles for your field
+</style>
---
+++ src/app/fieldTemplates/Image.vue
@@ -0,0 +1,19 @@
+<template>
+  <img :src="imageSrc || require('@/app/assets/img/coast.jpg')" :class="fieldClass"/>
+</template>
+<script>
+export default {
+  props:{
+    src:'',
+    fieldClass:''
+  },
+  computed:{
+    imageSrc:function(){
+     return this.src
+    }
+  }
+}
+</script>
+<style lang="scss">
+// here you can add custom styles for your field
+</style>
